04bfbae320b802a48568a620f8369a99e50d5941
TonyTwoko <132404498+TonyTwoko@users.noreply.github.com>
2025-02-26 00:48:33 +0300

    v.1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Пара helloworld равны по ссылке ? - " + (bean==bean2));
+        System.out.println("Пара cat равны по ссылке ? - " + (cat==cat2));
     }
 }
